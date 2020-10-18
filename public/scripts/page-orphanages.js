8d91a29c4a93f5d3d10111fb25637286884e0fdb
Matheus van der Lemon <imlearningcodes@gmail.com>
2020-10-17 21:12:10 -0300

    day2

---
+++ public/scripts/page-orphanages.js
@@ -0,0 +1,5 @@
+const  map = L.map('mapid').setView([-16.6799,  -49.255], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
